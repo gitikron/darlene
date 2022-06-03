30e8769a5d474b736e6c3acd8294b7f4c5513163
gitikron <64581747+gitikron@users.noreply.github.com>
2022-06-03 20:04:40 +0700

    g

--- .history/vue.config_20220603200236.js
+++ .history/vue.config_20220603200236.js
@@ -2,8 +2,4 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
     transpileDependencies: true,
     lintOnSave: false,
-}), {
-    devServer: {
-        disableHostCheck: true
-    }
-};
+});
